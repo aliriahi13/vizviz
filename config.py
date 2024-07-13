8f2b0ce51df4ffc184deecae9886ac42bb8f9ca0
aliriahi13 <114065453+aliriahi13@users.noreply.github.com>
2024-07-13 13:16:18 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+Token = "6690024831:AAE0h5N4tQBytLq_B4DHWFOYS48-zQ2O7ws"
+
+
+
+db_config = {
+    "host": "localhost",
+    "user": "root",
+    "password": "Ali.131173",
+    "database": "agahi"
+}
+
+
+channels = ["@mostwanted_wolv", "@channel_jadid_wolv", "@vizviz_wolv"]
+
+
+admins = [6237508867]
